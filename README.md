0be8b141e618a5c1679fb3e0ae6c409e811a46a6
Andrey Petrov <ayp@delisinfo.ru>
2021-10-12 18:37:31 +0300

    LRU algorithm implemented

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Cache
 
+2021-10-12   3 p.m.
 First implementaion without LRU list
+
+2021-10-12   6 p.m.
+LRU implemeted
+
--- cache.cpp
+++ cache.cpp
@@ -1,24 +1,34 @@
+//--------------------------------------------------------------------------------
+//  Copyright: (C) 2021-2021 by Andrey Petrov. All rights reserved
+//--------------------------------------------------------------------------------
+//  Unit:        
+//  Cooperation: 
+//  Date:        2021-10-12
+//--------------------------------------------------------------------------------
+//  Description:
+// 
+//	CopyRignt LRU Cache with limited size
+//	When we add new element to the cache the least recently added element can by dropped
+//
+//--------------------------------------------------------------------------------
+
+
 #include "pch.h"
 #include <cassert>
 #include <iostream>
+#include <list>
 #include <map>
+#include <unordered_map>
 
 using namespace std;
-
-
-// 
-//	Cache with limited size
-//	When we add new element to the cache the least recently added element can by dropped
-//
-
-
 
 template <typename K, typename V> class Cache
 {
 protected:
-	size_t		m_nMaxSize;
-	map<K, V>	m_map;
-	//K&			m_rLast;
+	size_t					m_nMaxSize;
+	list<V>					m_list;	
+	unordered_map<K, V*>	m_map;
+	
 
 public:
 	Cache(size_t nMaxSize)
@@ -26,7 +36,7 @@ public:
 		m_nMaxSize = nMaxSize;
 	}
 	V*		Get(K key);
-	void	Put(K key , V value);
+	void	Put(K key , V value);	
 };
 
 template <typename K, typename V>
@@ -34,10 +44,26 @@ V* Cache<K, V>::Get(K key)
 {	
 	try
 	{
-		V& value = m_map.at(key);
-		return &value;
+		V* pVal = m_map.at(key);
+		
+		if (pVal)
+		{
+			V value = *pVal; // save it !
+
+			//  forget old position
+			typename list<V>::iterator it;  // consider 'using' next time
+			it = find(m_list.begin(), m_list.end(), value);
+			m_list.erase(it);
+
+			//  set new position
+			m_list.push_front(value);
+			m_map[key] = &m_list.front();
+
+			pVal = &m_list.front();
+		}
+		return pVal;
 	}
-	catch (std::exception ex)
+	catch (exception ex)
 	{
 		cout << ex.what() << endl;		
 	}
@@ -48,11 +74,48 @@ V* Cache<K, V>::Get(K key)
 template <typename K, typename V>
 void Cache<K, V>::Put(K key, V value)
 {
-	if (m_map.size() == m_nMaxSize)
+	// not present in cache 
+	if (m_map.find(key) == m_map.end())
 	{
-		m_map.erase(m_map.begin());
+		// cache is full 
+		if (m_list.size() == m_nMaxSize)
+		{
+			//delete least recently used element 
+			V value = m_list.back();			
+			typename unordered_map<K, V*>::iterator it;			
+			for (it = m_map.begin(); it != m_map.end(); it++)
+			{
+				if ( *(it->second) == value )
+				{
+					m_map.erase(it);
+					break;
+				}
+			}						
+			/* not defined for using inside templates
+			it = find_if(m_map.begin(), m_map.end(), 				
+													[&value](const pair<K, V*>& p) -> bool
+													{
+														return ( *(p.second) == value );
+													}				
+						);			
+			m_map.erase(it);
+			*/
+			m_list.pop_back();
+		}
 	}
-	m_map.insert(pair<K, V>(key, value));
+
+	// present in cache 
+	else
+	{
+		V* pVal = m_map[key];
+		typename list<V>::iterator it;
+		it = find(m_list.begin(), m_list.end(), (*pVal));		
+		m_list.erase( it );
+	}
+
+	// update reference 
+	m_list.push_front(value);
+	m_map[key] = &m_list.front();
 }
 
 
@@ -69,8 +132,6 @@ int main()
 		
 	pValue = cache.Get(1);
 	assert(pValue);
-	pValue = cache.Get(2);
-	assert(pValue);
 
 	pValue = cache.Get(3);
 	if (pValue == nullptr)
@@ -83,13 +144,18 @@ int main()
 	pValue = cache.Get(3);
 	assert(pValue);
 	pValue = cache.Get(1);
+	assert(pValue);
+
+	pValue = cache.Get(2);
 	if (pValue == nullptr)
 	{
-		std::cout << "Least recently inserted pair(1,1) erased" << endl;
+		std::cout << "Least recently used pair(2,2) erased" << endl;
 	}
 
 	std::cout << endl << "Hello Cache! finished\n" << endl;
 }
+
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
